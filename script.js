9f5ec8fabbf0fe520fb3e5439573321229b8f84a
Piterqle <pedrohenriquemendesmartins5@gmail.com>
2025-11-19 22:49:28 -0300

    Modal Verifica a Permissão

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 
 function getLocationInfo() {
+    var modal = new bootstrap.Modal(document.getElementById('locationModal'));
     if(!navigator.geolocation) {
         console.log("Geolocation is not supported by your browser");
         return;
     }
-    console.log("Locating...");
     navigator.geolocation.getCurrentPosition(
         function(position) {
+            modal.hide();
             const latitude  = position.coords.latitude;
             const longitude = position.coords.longitude;
             console.log(`Latitude: ${latitude} °, Longitude: ${longitude} °`);  
         },
         function() {
+            modal.show();
             console.log("Erro em recuperar sua localização");
         }
     );
